35c1b4236f0b790b526258c12abff48c08fe1fa5
Samuel Wong <samuelwong779@gmail.com>
2023-11-21 23:14:56 -0800

    Changed buttons to 'Next' and added weight/height

--- src/stroke_application.py
+++ src/stroke_application.py
@@ -10,6 +10,8 @@ user_input={
 	'work_type' : None,
 	'Residence_type' : None,
 	'avg_glucose_level' : None,
+	'height': None,
+	'weight': None,
 	'bmi' : None,
 	'smoking_status' : None
 }
@@ -33,7 +35,7 @@ class strokeApp(tk.Tk):
 
 		# Creating dictionary of frames 
 		self.frames = {} 
-		for F in (MainMenu, Q1_Gender, Q2_Age, Q3_Hypertension, Q4_HeartDisease, Q5_EverMarried, Q6_WorkType, Q7_ResidenceType, Q8_AvgGlucoseLevel, Q9_BMI, Q10_SmokingStatus, Results):
+		for F in (MainMenu, Q1_Gender, Q2_Age, Q3_Hypertension, Q4_HeartDisease, Q5_EverMarried, Q6_WorkType, Q7_ResidenceType, Q8_AvgGlucoseLevel, Q9_Height, Q10_Weight, Q11_SmokingStatus, Results):
 			frame = F(container, self, user_input)
 			self.frames[F] = frame 
 			frame.grid(row = 0, column = 0, sticky ="nsew")
@@ -81,7 +83,7 @@ class Q1_Gender(tk.Frame):
 		rb_gender_NA = ttk.Radiobutton(self, text="Prefer not to say", variable=var, value=3).pack(padx=20, pady=20)
 		
 		# Go to question 2
-		btn_next = ttk.Button(self, text ="Question 2",command = lambda : self.next_question(var.get(), controller)).pack()
+		btn_next = ttk.Button(self, text ="Next",command = lambda : self.next_question(var.get(), controller)).pack()
 							
 	def next_question(self, value, controller):
 		if value == -1:
@@ -100,7 +102,7 @@ class Q2_Age(tk.Frame):
 		scale_age = tk.Scale(self, from_=1, to=110, orient=tk.HORIZONTAL, variable=var).pack(padx=20, pady=20)
 
 		# Go to question 3
-		btn_next = ttk.Button(self, text ="next", command = lambda : self.next_question(var.get(), controller)).pack()
+		btn_next = ttk.Button(self, text ="Next", command = lambda : self.next_question(var.get(), controller)).pack()
 							
 	def next_question(self, value, controller):
 		self.input['age'] = value
@@ -118,7 +120,7 @@ class Q3_Hypertension(tk.Frame):
 		rb_no = ttk.Radiobutton(self, text="No", variable=var, value=0).pack(padx=20, pady=20)
 
 		# Go to question 4
-		btn_next = ttk.Button(self, text ="next", command = lambda : self.next_question(var.get(), controller)).pack()
+		btn_next = ttk.Button(self, text ="Next", command = lambda : self.next_question(var.get(), controller)).pack()
 							
 	def next_question(self, value, controller):
 		if value == -1:
@@ -139,7 +141,7 @@ class Q4_HeartDisease(tk.Frame):
 		rb_no = ttk.Radiobutton(self, text="No", variable=var, value=0).pack(padx=20, pady=20)
 
 		# Go to question 5
-		btn_next = ttk.Button(self, text ="next", command = lambda : self.next_question(var.get(), controller)).pack()
+		btn_next = ttk.Button(self, text ="Next", command = lambda : self.next_question(var.get(), controller)).pack()
 							
 	def next_question(self, value, controller):
 		if value == -1:
@@ -160,7 +162,7 @@ class Q5_EverMarried(tk.Frame):
 		rb_no = ttk.Radiobutton(self, text="No", variable=var, value=0).pack(padx=20, pady=20)
 
 		# Go to question 6
-		btn_next = ttk.Button(self, text ="next", command = lambda : self.next_question(var.get(), controller)).pack()
+		btn_next = ttk.Button(self, text ="Next", command = lambda : self.next_question(var.get(), controller)).pack()
 							
 	def next_question(self, value, controller):
 		if value == -1:
@@ -184,7 +186,7 @@ class Q6_WorkType(tk.Frame):
 		rb_never_worked = ttk.Radiobutton(self, text="Never worked", variable=var, value='Never_worked').pack(padx=20, pady=20)
 
 		# Go to question 7
-		btn_next = ttk.Button(self, text ="next", command = lambda : self.next_question(var.get(), controller)).pack()
+		btn_next = ttk.Button(self, text ="Next", command = lambda : self.next_question(var.get(), controller)).pack()
 							
 	def next_question(self, value, controller):
 		if value == "":
@@ -205,7 +207,7 @@ class Q7_ResidenceType(tk.Frame):
 		rb_rural = ttk.Radiobutton(self, text="Rural", variable=var, value='Rural').pack(padx=20, pady=20)
 
 		# Go to question 8
-		btn_next = ttk.Button(self, text ="next", command = lambda : self.next_question(var.get(), controller)).pack()
+		btn_next = ttk.Button(self, text ="Next", command = lambda : self.next_question(var.get(), controller)).pack()
 							
 	def next_question(self, value, controller):
 		if value == "":
@@ -224,31 +226,54 @@ class Q8_AvgGlucoseLevel(tk.Frame):
 		scale_age = tk.Scale(self, from_=50, to=275, orient=tk.HORIZONTAL, variable=var).pack(padx=20, pady=20)
 
 		# Go to question 9
-		btn_next = ttk.Button(self, text ="next", command = lambda : self.next_question(var.get(), controller)).pack()
+		btn_next = ttk.Button(self, text ="Next", command = lambda : self.next_question(var.get(), controller)).pack()
 							
 	def next_question(self, value, controller):
 		self.input['avg_glucose_level'] = value
-		controller.show_frame(Q9_BMI, self.input)
+		controller.show_frame(Q9_Height, self.input)
 
-class Q9_BMI(tk.Frame):
+# TO-DO: Need to fix positioning
+class Q9_Height(tk.Frame):
 	def __init__(self, parent, controller, input):
 		tk.Frame.__init__(self, parent)
 		self.input = input
 
 		# Q9 widgets
-		lbl_question = ttk.Label(self, text ="What is your BMI?").pack(padx= 20, pady=20)
-		var = tk.IntVar()
-		scale_age = tk.Scale(self, from_=2, to=100, orient=tk.HORIZONTAL, variable=var).pack(padx=20, pady=20)
+		feet_options = ["4'", "5'", "6'", "7'"]
+		inches_options = ['0"', '1"', '2"', '3"', '4"', '5"', '6"', '7"', '8"', '9"', '10"', '11"']
 
+		lbl_question = ttk.Label(self, text ="What is your height in ft?").grid(row=0, column=0, sticky='ew')
+		var_feet = tk.StringVar()
+		var_inches = tk.StringVar()
+
+		om_feet = tk.OptionMenu(self, var_feet, *feet_options).grid(row=1, column=1, padx=5, pady=20)
+		om_inches = tk.OptionMenu(self, var_inches, *inches_options).grid(row=1, column=2, pady=20)
 		# Go to question 10
-		btn_next = ttk.Button(self, text ="next", command = lambda : self.next_question(var.get(), controller)).pack()
+		btn_next = ttk.Button(self, text ="Next", command = lambda : self.next_question(var_feet.get() + var_inches.get(), controller)).grid(row=2, column=1, columnspan=2)
 							
 	def next_question(self, value, controller):
-		self.input['bmi'] = value
-		controller.show_frame(Q10_SmokingStatus, self.input)
+		self.input['height'] = value
+		controller.show_frame(Q10_Weight, self.input)
+
+class Q10_Weight(tk.Frame):
+	def __init__(self, parent, controller, input):
+		tk.Frame.__init__(self, parent)
+		self.input = input
+
+		# Q9 widgets
+		lbl_question = ttk.Label(self, text ="What is your weight in lb?").pack(padx= 20, pady=20)
+		var = tk.IntVar()
+		scale_age = tk.Scale(self, from_=40, to=450, orient=tk.HORIZONTAL, variable=var).pack(padx=20, pady=20)
+
+		# Go to question 10
+		btn_next = ttk.Button(self, text ="Next", command = lambda : self.next_question(var.get(), controller)).pack()
+							
+	def next_question(self, value, controller):
+		self.input['weight'] = value
+		controller.show_frame(Q11_SmokingStatus, self.input)
 
 
-class Q10_SmokingStatus(tk.Frame):
+class Q11_SmokingStatus(tk.Frame):
 	def __init__(self, parent, controller, input):
 		tk.Frame.__init__(self, parent)
 		self.input = input
@@ -261,7 +286,7 @@ class Q10_SmokingStatus(tk.Frame):
 		rb_smokes = ttk.Radiobutton(self, text="Smokes", variable=var, value='smokes').pack(padx=20, pady=20)
 
 		# Go to question 10
-		btn_next = ttk.Button(self, text ="next", command = lambda : self.next_question(var.get(), controller)).pack()
+		btn_next = ttk.Button(self, text ="Next", command = lambda : self.next_question(var.get(), controller)).pack()
 							
 	def next_question(self, value, controller):
 		if value == "":
@@ -277,8 +302,23 @@ class Results(tk.Frame):
 							
 	def next_question(self, controller):
 		controller.show_frame(MainMenu)
+
 	
+	def BMI_calculator(self, ft_height, lb_weight):
+		# Convert lb to kg
+		kg_weight = int(lb_weight)/2.20462
+
+		# Convert feet to meters
+		feet = ft_height.rstrip('\"').split("\'", 1)[0]
+		inches = ft_height.split("\"", 1)[0]
+		inches = inches.split("\'", 1)[1]
+		inches = int(inches) + (int(feet) * 12)
+		meters = inches * 0.0254
+
+		return round(kg_weight/(meters**2), 1)
+
 	def show_results(self):
+		self.input['bmi'] = self.BMI_calculator(self.input['height'], self.input['weight'])
 		results = ttk.Label(self, text = 'Here are your results\n' + str(self.input), wraplength=250, justify='left').pack(padx= 20, pady=20)
 
 # Start application
