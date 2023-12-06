6a6c299e30cb97a6bd77ba5e3d81c2d428b3d33a
Luna Sang <dsa133@sfu.ca>
2023-12-05 18:04:15 -0800

    update model_stats to output results to output folder

--- src/model_stats.py
+++ src/model_stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+import os
 
 from joblib import load
 import pandas as pd 
@@ -32,45 +33,72 @@ y_test = test_data['stroke']
 
 models = [decision_tree, logistic_regression, naive_bayes, random_forest, voting_classifier_hard, voting_classifier_soft]
 
+# Output directory and file name
+output_directory = "./../output"
+output_filename = "model_stats.txt"
+output_path = os.path.join(output_directory, output_filename)
+
+# Create the output directory if it doesn't exist
+os.makedirs(output_directory, exist_ok=True)
+
 # Overall accuracy
 i = 0
 print('Accuracy\n--------')
-for model in models:
-    y_pred = model.predict(X_test)
-    print(f'{model_names[i]} {accuracy_score(y_test, y_pred)}')
-    i += 1
+
+with open(output_path, 'w') as file:
+    file.write('Accuracy\n--------\n')
+
+with open(output_path, 'a') as file:
+    for model in models:
+        y_pred = model.predict(X_test)
+        print(f'{model_names[i]} {accuracy_score(y_test, y_pred)}')
+        file.write(f'{model_names[i]} {accuracy_score(y_test, y_pred)}\n')
+        i += 1
 
 # Classification report
 i = 0
 print('Classification report\n--------')
-for model in models:
-    y_pred = model.predict(X_test)
-    print(f'{model_names[i]}\n {classification_report(y_test, y_pred)}')
-    i += 1
+with open(output_path, 'a') as file:
+    file.write('\nClassification report\n--------\n')
+    for model in models:
+        y_pred = model.predict(X_test)
+        print(f'{model_names[i]}\n {classification_report(y_test, y_pred)}')
+        file.write(f'{model_names[i]}\n {classification_report(y_test, y_pred)}\n')
+        i += 1
 
 # Confusion matrix
 i = 0
 print('\nConfusion Matrix\n--------')
-for model in models:
-    y_pred = model.predict(X_test)
-    cm = confusion_matrix(y_test, y_pred)
-    cm_display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
-    cm_display.plot()
-    plt.title(model_names[i])
-    plt.show()
-    i += 1
+with open(output_path, 'a') as file:
+    file.write('\nConfusion Matrix\n--------\n')
+    for model in models:
+        y_pred = model.predict(X_test)
+        cm = confusion_matrix(y_test, y_pred)
+        print(f'{model_names[i]}\n {cm}')
+        file.write(f'{model_names[i]}\n {cm}\n')
+        cm_display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
+        cm_display.plot()
+        plt.title(model_names[i])
+        plt.savefig(f'../output/{model_names[i]}_confusion_matrix.png')
+        plt.show()
+        i += 1
 
 # TPR and TNR 
 i = 0
 print('\nPositive and negative rates\n--------')
-for model in models:
-    y_pred = model.predict(X_test)
-    cm = confusion_matrix(y_test, y_pred)
-    TP = cm[1][1]
-    TN = cm[0][0]
-    FP = cm[0][1]
-    FN = cm[1][0]
-    print(model_names[i])
-    print(f"TPR rate: {round(TP/(TP + FN), 2) * 100}%")
-    print(f"TNR rate: {round(TN/(TN + FP), 2) * 100}%\n")
-    i += 1
+with open(output_path, 'a') as file:
+    file.write('\nPositive and negative rates\n--------\n')
+    for model in models:
+        y_pred = model.predict(X_test)
+        cm = confusion_matrix(y_test, y_pred)
+        TP = cm[1][1]
+        TN = cm[0][0]
+        FP = cm[0][1]
+        FN = cm[1][0]
+        print(model_names[i])
+        file.write(f'{model_names[i]}\n')
+        print(f"TPR rate: {round(TP/(TP + FN), 2) * 100}%")
+        file.write(f"TPR rate: {round(TP/(TP + FN), 2) * 100}%\n")
+        print(f"TNR rate: {round(TN/(TN + FP), 2) * 100}%\n")
+        file.write(f"TNR rate: {round(TN/(TN + FP), 2) * 100}%\n")
+        i += 1
